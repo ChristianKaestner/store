96141a6c8ad83f7270818107f472129d0918aa3b
Vladyslav Rohalov <domus.lg@gmail.com>
2023-08-18 12:49:29 +0200

    Added Local Storage Persistor

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,10 +1,42 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './auth/slice';
 import productsSlice from './products/slice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const productsPersistConfig = {
+  key: 'products',
+  storage,
+  whitelist: ['cart', 'favorite'],
+};
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice,
-    products: productsSlice,
+    auth: persistReducer(authPersistConfig, authSlice),
+    products: persistReducer(productsPersistConfig, productsSlice),
   },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
 });
+
+export const persistor = persistStore(store);

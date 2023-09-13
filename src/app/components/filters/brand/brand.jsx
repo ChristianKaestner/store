c585f412e84b472e2e1005d7851bb20cc373f424
Vladyslav Rohalov <domus.lg@gmail.com>
2023-09-13 17:49:57 +0200

    Updated brands
    1.Added useForm hook

--- src/app/components/filters/brand/brand.jsx
+++ src/app/components/filters/brand/brand.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-// import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useDebounce } from 'use-debounce';
 import FilterCommon from '../accordion/accordionCommon';
 import { Box, TextField, Checkbox, Typography } from '@mui/material';
@@ -18,13 +18,12 @@ export default function BrandFilter({ items }) {
     'brand'
   );
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm({
-  //   mode: 'onSubmit',
-  // });
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onSubmit',
+  });
 
   const handleChecked = e => {
     console.log(e.target.checked);
@@ -42,7 +41,9 @@ export default function BrandFilter({ items }) {
             type="search"
             size="small"
             value={searchedBrand}
-            onChange={e => setSearchedBrand(e.target.value)}
+            {...register('brandList', {
+              onChange: e => setSearchedBrand(e.target.value),
+            })}
           />
         </Box>
 
@@ -56,7 +57,14 @@ export default function BrandFilter({ items }) {
                   )}
                   <Label
                     control={
-                      <Checkbox value={brand} sx={{ p: 1 }} size="small" />
+                      <Checkbox
+                        value={brand}
+                        sx={{ p: 1 }}
+                        size="small"
+                        {...register('brandName', {
+                          onChange: handleChecked,
+                        })}
+                      />
                     }
                     label={
                       <Box sx={{ display: 'flex', flexDirection: 'row' }}>
@@ -65,7 +73,6 @@ export default function BrandFilter({ items }) {
                         </Counter>
                       </Box>
                     }
-                    onClick={handleChecked}
                   />
                 </Box>
               );

095d5745f46fcf2453ba8f5e69a136c2b1644973
Vladyslav Rohalov <domus.lg@gmail.com>
2023-09-18 19:57:34 +0200

    Added params handling to type filter

--- src/app/components/filters/type/type.jsx
+++ src/app/components/filters/type/type.jsx
@@ -1,19 +1,52 @@
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { getSearchParams } from '@/app/lib/functions';
 import { useForm } from 'react-hook-form';
 import FilterCommon from '../accordion/accordionCommon';
 import { Checkbox, Box } from '@mui/material';
 import { Form, List, Label } from '@/app/lib/commonStyles';
-import { debounce } from 'lodash';
+// import { debounce } from 'lodash';
 import { visuallyHidden } from '@mui/utils';
 import { Typography } from '@mui/material';
 
 export default function TypeFilter({ items }) {
   const { register, getValues } = useForm();
 
-  const handleChecked = debounce(() => {
-    // send request
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const paramsType = getSearchParams(searchParams, 'type');
+
+  const handleChecked = e => {
+    const value = e.target.value;
+    const current = new URLSearchParams(Array.from(searchParams.entries()));
+    const types = current.get('type');
+
+    if (types) {
+      const typeArray = types.split(',');
+
+      if (typeArray.includes(value)) {
+        typeArray.splice(typeArray.indexOf(value), 1);
+      } else {
+        typeArray.push(value);
+      }
+
+      if (typeArray.length > 0) {
+        current.set('type', typeArray.join(','));
+      } else {
+        current.delete('type');
+      }
+    } else {
+      current.set('type', value);
+    }
+
+    const search = decodeURIComponent(current.toString());
+    const query = search ? `?${search}` : '';
+    router.push(`${pathname}${query}`, { scroll: false });
+
+    //send request with values
     const values = getValues('type');
     console.log(values);
-  }, 1000);
+  };
 
   return (
     <FilterCommon title="Type">
@@ -23,11 +56,16 @@ export default function TypeFilter({ items }) {
       <Form component="form">
         <List component="ul" sx={{ pl: 2 }}>
           {items.map(({ id, type }) => {
+            let checked = false;
+            if (paramsType.includes(type)) {
+              checked = true;
+            }
             return (
               <Box component="li" key={id}>
                 <Label
                   control={
                     <Checkbox
+                      checked={checked}
                       value={type}
                       sx={{ p: 1 }}
                       size="small"

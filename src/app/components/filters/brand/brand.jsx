48e14fc25e714e08af86eb756c021379a08ce5f5
Vladyslav Rohalov <domus.lg@gmail.com>
2023-09-22 12:50:04 +0200

    Update brand filter
    1. Delete react hook use form
    2. Added dispatch methods
    3. Delete handling params logic

--- src/app/components/filters/brand/brand.jsx
+++ src/app/components/filters/brand/brand.jsx
@@ -1,27 +1,21 @@
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
 import { useDebounce } from 'use-debounce';
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
-import FilterCommon from '../accordion/accordionCommon';
-import { Box, TextField, Checkbox, Typography } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { addFilter, removeFilter } from '@/app/redux/filters/slice';
+import { useFilters } from '@/app/hooks/useFilters';
+import AccordionCommon from '../accordion/accordionCommon';
 import { addAlphabetIndex, filterByInput } from '@/app/lib/functions';
-import { getSearchParams } from '@/app/lib/functions';
+import { Box, TextField, Checkbox, Typography } from '@mui/material';
 import { Counter } from './brand.styled';
-import { Form, Label, ContainerFilter } from '@/app/lib/commonStyles';
-// import { debounce } from 'lodash';
+import { Form, Label, ContainerFilter, Row } from '@/app/lib/commonStyles';
 import { visuallyHidden } from '@mui/utils';
-import { Row } from '@/app/lib/commonStyles';
 
 export default function BrandFilter({ items }) {
   const [searchedBrand, setSearchedBrand] = useState('');
   const [debouncedBrand] = useDebounce(searchedBrand, 500);
 
-  const { register, getValues } = useForm();
-
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const paramsBrand = getSearchParams(searchParams, 'brand');
+  const { brand } = useFilters();
+  const dispatch = useDispatch();
 
   const brandsWithLetter = addAlphabetIndex(items, 'brand');
   const filtredBrands = filterByInput(
@@ -30,37 +24,23 @@ export default function BrandFilter({ items }) {
     'brand'
   );
 
-  const handleChecked = e => {
-    const value = e.currentTarget.value.toLowerCase();
-    const current = new URLSearchParams(Array.from(searchParams.entries()));
-    const brands = current.get('brand');
-
-    if (brands) {
-      const brandArray = brands.split(',');
-      if (brandArray.includes(value)) {
-        brandArray.splice(brandArray.indexOf(value), 1);
-      } else {
-        brandArray.push(value);
-      }
-      if (brandArray.length > 0) {
-        current.set('brand', brandArray.join(','));
-      } else {
-        current.delete('brand');
-      }
-    } else {
-      current.set('brand', value);
-    }
-    const search = decodeURIComponent(current.toString());
-    const query = search ? `?${search}` : '';
-    router.push(`${pathname}${query}`, { scroll: false });
-
-    // send request with values
-    const values = getValues('brandName');
-    console.log(values);
+  const handleChecked = (checked, curentBrand) => {
+    checked
+      ? dispatch(
+          addFilter({
+            filterName: 'brand',
+            filterValue: curentBrand,
+          })
+        )
+      : dispatch(
+          removeFilter({
+            filterName: 'brand',
+            filterValue: curentBrand,
+          })
+        );
   };
-
   return (
-    <FilterCommon title="Brand">
+    <AccordionCommon title="Brand">
       <Typography component="h3" sx={visuallyHidden}>
         Search by brand
       </Typography>
@@ -72,44 +52,37 @@ export default function BrandFilter({ items }) {
             type="search"
             size="small"
             value={searchedBrand}
-            {...register('brandList', {
-              onChange: e => setSearchedBrand(e.target.value),
-            })}
+            onChange={e => setSearchedBrand(e.target.value)}
           />
         </Box>
 
         <ContainerFilter component="ul">
           {items.length > 0 &&
-            filtredBrands.map(({ id, brand, letter, count }) => {
-              let checked = false;
-              const brandMod = brand.toLowerCase();
-
-              if (paramsBrand.includes(brandMod)) {
-                checked = true;
-              }
+            filtredBrands.map(item => {
+              const { id, letter, count } = item;
               return (
                 <Box key={id} component="li">
                   {letter && (
                     <Typography sx={{ fontWeight: 500 }}>{letter}</Typography>
                   )}
                   <Label
-                    control={
-                      <Checkbox
-                        checked={checked}
-                        value={brand}
-                        sx={{ p: 1 }}
-                        size="small"
-                        {...register('brandName', {
-                          onChange: handleChecked,
-                        })}
-                      />
-                    }
                     label={
                       <Row>
                         <Counter badgeContent={count}>
-                          <Typography>{brand}</Typography>
+                          <Typography>{item.brand}</Typography>
                         </Counter>
                       </Row>
+                    }
+                    control={
+                      <Checkbox
+                        value={brand}
+                        checked={brand.includes(item.brand.toLowerCase())}
+                        sx={{ p: 1 }}
+                        size="small"
+                        onChange={(e, checked) => {
+                          handleChecked(checked, item.brand.toLowerCase());
+                        }}
+                      />
                     }
                   />
                 </Box>
@@ -117,6 +90,6 @@ export default function BrandFilter({ items }) {
             })}
         </ContainerFilter>
       </Form>
-    </FilterCommon>
+    </AccordionCommon>
   );
 }

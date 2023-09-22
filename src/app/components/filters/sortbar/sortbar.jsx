d469a5941216ca30ffe0a9688212813007efdf07
Vladyslav Rohalov <domus.lg@gmail.com>
2023-09-22 12:16:03 +0200

    Update type filter
    1. Delete react hook use form
    2. Added dispatch methods
    3. Delete handling params logic

--- src/app/components/filters/sortbar/sortbar.jsx
+++ src/app/components/filters/sortbar/sortbar.jsx
@@ -22,10 +22,9 @@ export default function Sortbar() {
   const searchParams = useSearchParams();
   const dispatch = useDispatch();
   const isMount = useIsMount();
-  console.log(filters);
+
   const updateFilters = () => {
     const queryParams = new URLSearchParams(Array.from(searchParams.entries()));
-
     // check redux state and update param
     Object.keys(filters).forEach(filterName => {
       const filterValue = filters[filterName];
@@ -104,84 +103,3 @@ export default function Sortbar() {
     </Box>
   );
 }
-
-// import { getSearchParams } from '@/app/lib/functions';
-// import { FilterBlock, FilterBtn, IconClose } from './sortbar.styled';
-// import { Box } from '@mui/material';
-// import { TextBold } from '@/app/lib/commonStyles';
-
-// const filtersState = useFilters();
-// console.log(filtersState);
-// const filtersKey = [
-//   'brand',
-//   'color',
-//   'price',
-//   'size',
-//   'status',
-//   'type',
-//   'weight',
-//   'flavor',
-// ];
-// const filters = [];
-// const router = useRouter();
-// const pathname = usePathname();
-// const searchParams = useSearchParams();
-// const current = new URLSearchParams(Array.from(searchParams.entries()));
-// filtersKey.forEach(key => {
-//   const param = getSearchParams(searchParams, key);
-//   if (param.length) {
-//     param.forEach(n => filters.push({ name: key, value: n }));
-//   }
-// });
-// // console.log(filters);
-// const handleDelete = e => {
-//   const name = e.currentTarget.id;
-//   const value = e.currentTarget.textContent;
-//   const param = current.get(name);
-//   const paramArray = param.split(',');
-//   if (paramArray.includes(value)) {
-//     paramArray.splice(paramArray.indexOf(value), 1);
-//   }
-//   if (paramArray.length > 0) {
-//     current.set(name, paramArray.join(','));
-//   } else {
-//     current.delete(name);
-//   }
-// const search = decodeURIComponent(current.toString());
-// const query = search ? `?${search}` : '';
-// router.push(`${pathname}${query}`, { scroll: false });
-// };
-// const handleDeleteAll = () => {
-//   router.push(pathname, { scroll: false });
-// };
-// return (
-//   <Box>
-//     {filters.length > 0 && (
-//       <FilterBlock component="ul" sx={{ gap: 1, flexWrap: 'wrap' }}>
-//         <TextBold>30 products found</TextBold>
-//         <FilterBtn
-//           variant="outlined"
-//           endIcon={<IconClose />}
-//           component="li"
-//           onClick={handleDeleteAll}
-//         >
-//           clear all
-//         </FilterBtn>
-//         {filters.map(({ name, value }) => {
-//           return (
-//             <FilterBtn
-//               key={value}
-//               id={name}
-//               variant="outlined"
-//               endIcon={<IconClose />}
-//               component="li"
-//               onClick={handleDelete}
-//             >
-//               {value}
-//             </FilterBtn>
-//           );
-//         })}
-//       </FilterBlock>
-//     )}
-//   </Box>
-// );
--- src/app/components/filters/type/type.jsx
+++ src/app/components/filters/type/type.jsx
@@ -1,87 +1,60 @@
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
-import { getSearchParams } from '@/app/lib/functions';
-import { useForm } from 'react-hook-form';
-import FilterCommon from '../accordion/accordionCommon';
-import { Checkbox, Box } from '@mui/material';
-import { Form, List, Label } from '@/app/lib/commonStyles';
-// import { debounce } from 'lodash';
+import { useDispatch } from 'react-redux';
+import { addFilter, removeFilter } from '@/app/redux/filters/slice';
+import { useFilters } from '@/app/hooks/useFilters';
+import AccordionCommon from '../accordion/accordionCommon';
+import { Checkbox, Box, Typography } from '@mui/material';
+import { Form, Label, List } from '@/app/lib/commonStyles';
 import { visuallyHidden } from '@mui/utils';
-import { Typography } from '@mui/material';
 
 export default function TypeFilter({ items }) {
-  const { register, getValues } = useForm();
+  const { type } = useFilters();
+  const dispatch = useDispatch();
 
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const paramsType = getSearchParams(searchParams, 'type');
-
-  const handleChecked = e => {
-    const value = e.currentTarget.value.toLowerCase();
-    const current = new URLSearchParams(Array.from(searchParams.entries()));
-    const types = current.get('type');
-
-    if (types) {
-      const typeArray = types.split(',');
-
-      if (typeArray.includes(value)) {
-        typeArray.splice(typeArray.indexOf(value), 1);
-      } else {
-        typeArray.push(value);
-      }
-
-      if (typeArray.length > 0) {
-        current.set('type', typeArray.join(','));
-      } else {
-        current.delete('type');
-      }
-    } else {
-      current.set('type', value);
-    }
-
-    const search = decodeURIComponent(current.toString());
-    const query = search ? `?${search}` : '';
-    router.push(`${pathname}${query}`, { scroll: false });
-
-    //send request with values
-    const values = getValues('type');
-    console.log(values);
+  const handleChecked = (checked, curentType) => {
+    checked
+      ? dispatch(
+          addFilter({
+            filterName: 'type',
+            filterValue: curentType,
+          })
+        )
+      : dispatch(
+          removeFilter({
+            filterName: 'type',
+            filterValue: curentType,
+          })
+        );
   };
 
   return (
-    <FilterCommon title="Type">
+    <AccordionCommon title="Type">
       <Typography component="h3" sx={visuallyHidden}>
         Search hookah bowl by type
       </Typography>
       <Form component="form">
         <List component="ul" sx={{ pl: 2 }}>
-          {items.map(({ id, type }) => {
-            let checked = false;
-            const typeMod = type.toLowerCase();
-            if (paramsType.includes(typeMod)) {
-              checked = true;
-            }
+          {items.map(item => {
             return (
-              <Box component="li" key={id}>
+              <Box component="li" key={item.id}>
                 <Label
+                  label={item.type}
                   control={
                     <Checkbox
-                      checked={checked}
                       value={type}
+                      checked={type.includes(item.type.toLowerCase())}
                       sx={{ p: 1 }}
                       size="small"
-                      {...register('type', {
-                        onChange: handleChecked,
-                      })}
+                      onChange={(e, checked) => {
+                        handleChecked(checked, item.type.toLowerCase());
+                      }}
                     />
                   }
-                  label={type}
                 />
               </Box>
             );
           })}
         </List>
       </Form>
-    </FilterCommon>
+    </AccordionCommon>
   );
 }
